088feb2d2e49f7e551fdcba7bee49c0e5f0ca369
mcnutty26 <williamseymour123@googlemail.com>
2014-11-29 17:05:33 +0000

    Added discussion page, with the ability to comment on individual questions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 dissertation
 ============
+TODO:
+  show a breakdown of answers for a module
+  give an explanation when the user got it wrong
---
+++ code/discuss.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Will
+ * Date: 29/11/2014
+ * Time: 15:40
+ */
+require "core/user.php";
+session_start();
+$current_user = new user($_SESSION['user']);
+$user = $_SESSION['user'];
+$userid = $current_user->get_id();
+
+//Process actions
+if (isset($_POST['message'])) {
+    $message = database::escape($_POST['message']);
+    $c2 = $_SESSION['choice2'];
+    $query = "INSERT INTO ds_chat
+              (type, itemid, F_userid, message)
+              VALUES
+              ('CONTENT', $c2, $userid, '$message')";
+    database::query($query);
+
+}
+
+//Process display
+
+$query =    "SELECT name, moduleid
+             FROM ds_module AS m";
+
+$result = database::query($query);
+if (!isset($_POST['choice'])) {
+    echo '? -> ?';
+    ?>
+    <form action="discuss.php" method="post">
+        <select name="choice">
+            <? while ($row = mysqli_fetch_array($result)) {
+                echo '<option value="' . $row['moduleid'] . '">' . $row['name'] . '</option>';
+            } ?>
+        </select>
+        <input type="submit" value="Submit">
+    </form>
+<?
+} else if (!isset($_POST['choice2'])) {
+    $query =    "SELECT name
+             FROM ds_module
+             WHERE moduleid = " . $_POST['choice'];
+    $result = database::query($query);
+    $choice = '';
+    while ($row = mysqli_fetch_array($result)) {
+        $_SESSION['choice'] = $row['name'];
+    }
+
+    echo $_SESSION['choice'] . ' -> ?';
+    $query =    "SELECT name, contentid
+             FROM ds_content AS c
+             WHERE F_moduleid = " . $_POST['choice'];
+
+    $result = database::query($query);
+    ?>
+    <form action="discuss.php" method="post">
+        <select name="choice2">
+            <? while($row = mysqli_fetch_array($result)) {
+                echo '<option value="' . $row['contentid'] . '">' . $row['name'] . '</option>';
+            } ?>
+        </select>
+        <input type="hidden" name="choice" value="<? echo $_POST['choice']; ?>">
+        <input type="submit" value="Submit">
+    </form>
+    <?
+} else {
+    $query =    "SELECT name
+             FROM ds_content AS m
+             WHERE contentid = " . $_POST['choice2'];
+    $result = database::query($query);
+    $choice2 = '';
+    while ($row = mysqli_fetch_array($result)) {
+        $choice2 = $row['name'];
+    }
+
+    echo $_SESSION['choice'] . ' -> ' . $choice2;
+    $_SESSION['choice2'] = $_POST['choice2'];
+
+    echo "<br><br>Current talk:";
+    $query =    "SELECT u.name, c.message
+                 FROM ds_chat AS c
+                 INNER JOIN ds_user AS u ON c.F_userid = u.userid
+                 WHERE c.type = 'CONTENT'
+                 AND itemid =" . $_POST['choice2'];
+    $result = database::query($query);
+    echo '<table border=1>';
+    echo '<tr><td>User</td><td>Message</td></tr>';
+    while ($row = mysqli_fetch_array($result)) {
+        echo '<tr><td>'. $row['name'] . '</td><td>' . $row['message'] . '</td></tr>';
+    }
+    echo '</table>';
+    echo '<br>Have your say:<br>'; ?>
+    <form action="discuss.php" method="post">
+        <input type="text" name="message"><br>
+        <input type="hidden" name="choice" value="<? echo $_POST['choice']; ?>">
+        <input type="hidden" name="choice2" value="<? echo $_POST['choice2']; ?>">
+        <input type="submit" value="Post">
+    </form>
+<? } ?>
