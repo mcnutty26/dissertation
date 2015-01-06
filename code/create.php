d83200e3a26d3cf6312737d7a575d9e72e918c14
mcnutty26 <williamseymour123@googlemail.com>
2015-01-06 16:35:08 +0000

    Started adding tags, began thinking about report chapters

--- code/create.php
+++ code/create.php
@@ -45,9 +45,9 @@ if ($_POST['action'] == 'content') {
     if (isset($_POST['module'])) {
         $module = database::escape($_POST['module']);
         $result = database::query("SELECT * FROM ds_content WHERE F_moduleid = $module");
-        echo '<table><thead><tr><td>Question ID</td><td>Name</td><td>Question</td><td>Edit?</td></tr></thead>';
+        echo '<table><thead><tr><td>Question ID</td><td>Name</td><td>Question</td><td>Tags</td><td>Edit?</td></tr></thead>';
         while($row = mysqli_fetch_array($result)) {
-            echo '<tr><td>' . $row['contentid'] . '</td><td>' . $row['name'] . '</td><td>' . $row['question'] . '</td><td><form action="create.php" method="post"><input type="hidden" name="action" value="answer"><input type="hidden" name="content" value="' . $row['contentid'] . '"><input type="submit" value="Edit"></form></td></tr>';
+            echo '<tr><td>' . $row['contentid'] . '</td><td>' . $row['name'] . '</td><td>' . $row['question'] . '</td><td>' . $row['tag'] . '</td><td><form action="create.php" method="post"><input type="hidden" name="action" value="answer"><input type="hidden" name="content" value="' . $row['contentid'] . '"><input type="submit" value="Edit"></form></td></tr>';
         }
         echo '<tr><form action="create.php" method="post"><input type="hidden" name="module" value="' . $module . '"><input type="hidden" name="action" value="content"><td>-</td><td><input type="text" name="newC" placeholder="New Question Name"></td><td><input type="text" name="newQ"></td><td><input type="submit" value="Save"></td></form></tr>';
         echo '</table>';
@@ -74,9 +74,9 @@ if ($_POST['action'] == 'content') {
     $result = database::query("SELECT * FROM ds_module");
     //$result2 = database::query("SELECT * FROM ds_content WHERE F_moduleid =" . )
     if ($result) {
-        echo '<table><thead><tr><td>Module ID</td><td>Name</td><td>Content</td><td>Edit?</td><td>Class stats</td></tr></thead>';
+        echo '<table><thead><tr><td>Module ID</td><td>Name</td><td>Edit?</td><td>Class stats</td></tr></thead>';
         while($row = mysqli_fetch_array($result)) {
-            echo '<tr><td>' . $row['moduleid'] . '</td><td>' . $row['name'] . '</td><td>TODO</td><td><form action="create.php" method="post"><input type="hidden" name="action" value="content"><input type="hidden" name="module" value="' . $row['moduleid'] . '"><input type="submit" value="Edit"></form></td><td><form action="dashboard.php" method="post"><input type="hidden" name="module" value="' . $row['moduleid'] . '"><input type="submit" value="Stats"></form></td></tr>';
+            echo '<tr><td>' . $row['moduleid'] . '</td><td>' . $row['name'] . '</td><td><form action="create.php" method="post"><input type="hidden" name="action" value="content"><input type="hidden" name="module" value="' . $row['moduleid'] . '"><input type="submit" value="Edit"></form></td><td><form action="dashboard.php" method="post"><input type="hidden" name="module" value="' . $row['moduleid'] . '"><input type="submit" value="Stats"></form></td></tr>';
         }
         echo '<tr><form action="create.php" method="post"><td>-</td><td><input type="text" name="newM" placeholder="New Module Name"></td><td>None ... yet!</td><td><input type="submit" value="Save"></td></form></tr>';
         echo '</table>';
